46e4dcc0f1e2115b27232db587d7a6581e27898e
fadhlanfm <fafadhlanriz@gmail.com>
2019-12-28 15:50:17 +0700

    edit error message (kalo counter==0)

--- application/views/admin_pages/dashboard.php
+++ application/views/admin_pages/dashboard.php
@@ -4,7 +4,7 @@
     <div class="intro-container wow fadeIn">
       <h1 class="mb-4 pb-0">Welcome,<br>You are<span>
       <?php if($isSuperAdmin==1){
-          echo "A Super Admin</span></h1>";
+          echo "An Admin</span></h1>";
       }else{
           echo "An Admin</span></h1>";
       } ?>
